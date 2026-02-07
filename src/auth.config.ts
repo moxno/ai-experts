356a198f714825168caaff20cf4c94b26854aaa0
Ilya Sib <ilsimox@gmail.com>
2026-02-07 18:01:01 +0200

    Refactor NextAuth configuration for improved callbacks

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,34 +1,44 @@
 import type { NextAuthConfig } from "next-auth"
 import Google from "next-auth/providers/google"
 
-
 const isProd = process.env.NODE_ENV === "production";
 const domain = "ai-experts.co.il";
 
-
 export default {
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-            allowDangerousEmailAccountLinking: true,
-        }),
-    ],
-    debug: true,
-    trustHost: true,
-    cookies: {
-        sessionToken: {
-            name: `__session`,
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                secure: true,
-            },
-        },
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  debug: true,
+  trustHost: true,
+  cookies: {
+    sessionToken: {
+      name: `__session`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
     },
-        callbacks: {
-        async session({ session, token }: any) {
-            if (session.user && token) {
-                session.user.id = token.id;
-                session.user.role = token.role;
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = (user as any).role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    }
+  }
+} satisfies NextAuthConfig
